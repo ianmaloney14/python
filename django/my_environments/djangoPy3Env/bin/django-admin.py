b56157ea9e9b2a37f73a9eb8942215db61f6153f
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2022-09-03 13:14:54 -0700

    first commit

--- django/my_environments/djangoPy3Env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/ianmaloney/Desktop/code/codingdojo/python/django/my_environments/djangoPy3Env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
